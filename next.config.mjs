a1d5cdc2f03c25e0f42bd15860cbd1346849e879
ArthurZack <arthurzack0915@gmail.com>
2026-09-01 10:30:17 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'minotar.net'
-      }
-    ]
-  }
+  basePath: '/bans',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'minotar.net'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
